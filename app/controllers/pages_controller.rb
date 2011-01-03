7a336175c42c0a8344a97321891b303ef0fe3ed4
Kleber <klebershimabuku@gmail.com>
2011-01-03 17:24:42 +0900

    title tests and blueprint css files added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contato"
   end
 
   def about
+    @title = "Sobre"
   end
 
   def help
+    @title = "Ajuda"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
+  before(:each) do
+    #define the @base_title content
+    @base_title = "ShigotoDOKO"
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Home")
     end
   end
 
@@ -14,6 +26,12 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Contato")
+    end
   end
 
   describe "GET 'about'" do
@@ -21,12 +39,24 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Sobre")
+    end
   end
 
   describe "GET 'help'" do
     it "should be successful" do
       get 'help'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Ajuda")
     end
   end
 
