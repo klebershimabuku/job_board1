ac9d9ca4f4a64f67203c0b84a23307045dd5385b
Kleber <klebershimabuku@gmail.com>
2011-01-03 13:27:30 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobBoard::Application.config.secret_token = '312f11f23bbc3bcb26eceec65b4d8c56e9142193c42e6f13701504870ae2ec5ce6577440068e80bcf7ceb31fa2897d7e564283bc85b3ca278d00101110aefe8f'
