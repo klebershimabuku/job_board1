3c3f7476d95801af57ba15a2e5aa4c2d0d73fd3f
Kleber <klebershimabuku@gmail.com>
2011-01-06 04:57:58 +0900

    added simple_form support

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.0.3'
 gem 'mysql2'
 gem 'devise'
 gem 'cancan'
+gem "simple_form"
 
 group :development do
   gem 'rspec-rails', '2.3.0'
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,12 +1,14 @@
 class Job < ActiveRecord::Base
 
-  validates :title,   :length => { :maximum => 50 }
-  validates :content, :length => { :maximum => 1000 }
+  validates :title,     :presence => true,    :length => { :maximum => 50 }
+  validates :location,  :presence => true
+  validates :content,   :presence => true, :length => { :maximum => 1000 }
 
   URL_REGEX = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
   validates :company_website, :format => URL_REGEX
+  validates :company_name, :presence => true
 
-  validates :how_to_apply, :length => { :maximum => 160 }
+  validates :how_to_apply, :presence => true, :length => { :maximum => 160 }
 
   belongs_to :user
 
