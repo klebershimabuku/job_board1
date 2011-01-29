77a2cbe4b559c778c803db581290070a2dd0a1fb
Kleber <klebershimabuku@gmail.com>
2011-01-29 14:35:44 +0900

    added job test

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -2,9 +2,20 @@ require 'spec_helper'
 
 describe Job do
   
-  it "should be valid" do
-    title.should be_valid
-  end
+  before(:each) do
+    @attr = {
+      :title => "Contrata-se para fabrica de autos",
+      :content => "Estamos contratando pessoas interessadas em trabalhar em fabrica de autos",
+      :location => "Aichi-ken, Toyohashi-shi",
+      :company_name => "K.K. Test",
+      :company_website => "http://www.test.com",
+      :how_to_apply => "Enviar email para contato@test.com"
+    }
+  end  
   
+  it "should require a title" do
+    job = Job.new(@attr.merge(:title => ''))
+    job.should_not be_valid
+  end
   
 end
