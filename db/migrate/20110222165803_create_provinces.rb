36bf5a5b9c1966bdab086a39add55f501e2802c7
Kleber <klebershimabuku@gmail.com>
2011-02-23 02:00:38 +0900

    added provinces model

---
+++ db/migrate/20110222165803_create_provinces.rb
@@ -0,0 +1,13 @@
+class CreateProvinces < ActiveRecord::Migration
+  def self.up
+    create_table :provinces do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :provinces
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110221140034) do
+ActiveRecord::Schema.define(:version => 20110222165803) do
 
   create_table "accounts", :force => true do |t|
     t.string   "active_pack"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(:version => 20110221140034) do
     t.boolean  "expired",           :default => false
     t.datetime "expired_at"
     t.boolean  "highlight"
+  end
+
+  create_table "provinces", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", :force => true do |t|
