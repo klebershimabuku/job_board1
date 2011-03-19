4abf77fbaea9482a5d1bf789b683c165da22c166
Kleber <klebershimabuku@gmail.com>
2011-03-20 02:00:43 +0900

    fix#3

--- config/initializers/setup_mail.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address  => "mail.shigotodoko.com",
-  :port  => 25,
-  :domain  => "shigotodoko.com",
-  :user_name  => "alert@shigotodoko.com",
-  :password  => "pqlamz09085166027",
-  :authentication  => :login
-}
