25cc9d36f4eaa401fc6c912c844c24d70e8ab1c2
Kleber <klebershimabuku@gmail.com>
2011-03-20 02:23:07 +0900

    fix mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,9 @@ JobBoard::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'shigotodoko.com' }
+  config.action_mailer.delivery_method = :smtp
+  
 
   # Enable threaded mode
   # config.threadsafe!
