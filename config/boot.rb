55bab4167de1c8c4e61ab91ca951ea02e5fba055
Kleber Shimabuku <klebershimabuku@gmail.com>
2011-10-01 02:10:39 +0900

    fix google plus data-href links and boot file

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'yaml'
 
-YAML::ENGINE.yamler = 'syck'
+YAML::ENGINE.yamler = 'syck' if defined?(YAML::ENGINE)
 
 # Set up gems listed in the Gemfile.
 gemfile = File.expand_path('../../Gemfile', __FILE__)
