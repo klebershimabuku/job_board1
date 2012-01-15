c9bfbd24ae51362a8c66b10bd45bdce217051f3b
Kleber Shimabuku <klebershimabuku@gmail.com>
2012-01-15 16:13:49 +0900

    unused files

--- spec/controllers/jobs_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe JobsController do
-  
-  context 'should validates the controller' do
-    it { JobsController.new.should be_an_instance_of(JobsController) }
-  end
-  
-end
