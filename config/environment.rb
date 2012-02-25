033714fc67e6d1d1ee9550c4c8e2617f296833e7
Kleber Shimabuku <klebershimabuku@gmail.com>
2012-02-25 17:44:20 +0900

    remove environment.rb

--- config/environment.rb
+++
@@ -1,9 +0,0 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-JobBoard::Application.initialize!
-
-ENV['RECAPTCHA_PUBLIC_KEY']  = '6LcebsgSAAAAAE-4kJsGAXnyywcKXyfPzHm0gj0n'
-ENV['RECAPTCHA_PRIVATE_KEY'] = '6LcebsgSAAAAAKbk_ChZezo6I7QJxdA8gk-KCJeg'
-
